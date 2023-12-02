43e9aecdf4e43de6304e6f154e2f76cef0ca67f6
tus0inoue1007 <4620007@ed.tus.ac.jp>
2023-12-03 00:54:58 +0900

    1202

---
+++ 1202_DAIWA/c.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cmath>
+using namespace std;
+#define rep(i,n) for (int i=0; i< n;i++)
+
+int main() {   
+
+
+    return 0;
+}
