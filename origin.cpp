b2985577d4f05eb6c105e0b38f4daf601c065b37
Ruri1212 <rikuinoue1007@icloud.com>
2024-02-18 15:26:39 +0900

    cppのコピー元

---
+++ origin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cmath>
+using namespace std;
+#define rep(i,n) for (int i=0; i< n;i++)
+
+// 考えたこと
+//
+
+
+int main() {   
+
+    return 0;
+}
