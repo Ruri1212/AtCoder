1f58e2e738b9eced3a1de3c24391188ca865fc41
Ruri1212 <rikuinoue1007@icloud.com>
2024-02-19 16:49:16 +0900

    cppの記法集

---
+++ techique.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+int main() {   
+
+    int vector<int> G[ ];
+    G[a].push_back(b);
+    for (int to : G[pos]){
+        
+    }
+
+
+    queue<int> Q;
+    Q.push(start);
+    int pos = Q.front();Q.pop();
+
+
+
+    return 0;
+}
