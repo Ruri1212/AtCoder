2f6a152aa15f569e4f1d6bc9928d4285494e9c60
Ruri1212 <rikuinoue1007@icloud.com>
2024-03-03 00:57:47 +0900

    2024_0302

---
+++ Atcoder_2024/0302/origin.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cmath>
+using namespace std;
+#define rep(i,n) for (int i=0; i< n;i++)
+
+int main() {   
+
+    int N;
+    cin >> N;
+
+    int ans;
+    int m;
+    m = 1e17
+
+    for( int i = 1; i<=m; i++){
+
+
+    }
+
+
+
+    return 0;
+}
